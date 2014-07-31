2105e96e467057459bb6337174263664212cf016
frk <vs1@frkeller.com>
2014-07-31 12:26:07 -0400

    Added first 2 lines.

---
+++ ConApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press the Enter key: ");
+            Console.ReadLine();
+        }
+    }
+}
