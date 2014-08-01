41e3549685a5bbbc5c9a40f6c4d4bcd535fd9d99
frk <vs1@frkeller.com>
2014-08-01 15:15:29 -0400

    Lot of demo statements.

---
+++ AddEmployee/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddEmployee
+{
+    public class Employee
+    {
+        public string id;
+        public string name;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public static int employeeCounter;
+
+        public static int AddEmployee()
+        {
+            return ++employeeCounter;
+        }
+    }
+
+    class EmployeeMainClass : Employee
+    {
+        public static void EmployeeMain()
+        {
+            Console.Write("Enter the current number of employees: ");
+            string n = Console.ReadLine();
+            Employee.employeeCounter = Int32.Parse(n);
+
+            Console.Write("Enter the new employee name : ");
+            string name = Console.ReadLine();
+            Console.Write("Enter the new employee ID   : ");
+            string id = Console.ReadLine();
+            
+            // Create the employee object:
+            Employee e = new Employee(name, id);
+            Employee.AddEmployee();
+            
+            // Display the new information:
+            Console.WriteLine("Name: {0}", e.name);
+            Console.WriteLine("ID:   {0}", e.id);
+            Console.WriteLine("New Number of Employees: {0}",
+                               Employee.employeeCounter);
+            Console.Write("Press the Enter key: ");
+            Console.ReadLine();
+        }
+    }
+}
--- ConApp2/Program1.cs
+++ ConApp2/Program1.cs
@@ -8,13 +8,62 @@ namespace ConApp3
 {
     class ConApp3Class1
     {
+        private static string sReply = "noreply";
+        private static int n1 = 123;
+        private static Int16 n2 = 456;
+
         static void Main(string[] args)
         {
             Console.WriteLine("ConApp3.ConApp3Class1.Main says Hello, world.");
-            Console.WriteLine("ConApp3.ConApp3Class1.Main says Press the Enter key: ");
+
+            Point pt1 = new Point();
+            Point pt2 = new Point(123, 456);
+
+            // Display results:
+            Console.Write("pt1: ");
+            Console.WriteLine("x = {0}, y = {1}", pt1.x, pt1.y);
+            Console.Write("pt2: ");
+            Console.WriteLine("x = {0}, y = {1}", pt2.x, pt2.y);
+            Console.WriteLine(pt1.GetType());
+            Console.WriteLine(sReply.GetType());
+            Console.WriteLine(n1.GetType());
+            Console.WriteLine(n2.GetType());
+            Console.WriteLine(n2.GetTypeCode());
+
+            string s1 = "\u0068ello ";
+            string s2 = "world";
+            char c0 = s1[0]; // should be "h"
+            char c1 = s1[1]; // should be "e"
+            Console.WriteLine(s1 + s2);
+            Console.WriteLine(s1 + s2 == "hello world");
+            Console.WriteLine(c0);
+            Console.WriteLine(c1);
+                        
+            do
+            {
+                Console.Write("Enter c to continue or q to quit: ");
+                sReply = Console.ReadLine();
+                if (sReply.StartsWith("q"))
+                    Environment.Exit(0);
+            } while ( ! sReply.StartsWith("c") );
+            
+            //ConApp3Class2.hello2();
+            //ConApp3Class3.hello3();
+            EmployeeMainClass.EmployeeMain();
+            Console.Write("ConApp3.ConApp3Class1.Main says Press the Enter key: ");
             Console.ReadLine();
-            ConApp3Class2.hello2();
-            ConApp3Class3.hello3();
         }
+
+        public struct Point 
+        {
+           public int x, y;
+
+           public Point(int p1, int p2) 
+           {
+              this.x = p1;
+              this.y = p2;    
+           }
+        }
+
     }
 }
