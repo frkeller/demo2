518f91ba195fe9b27e1e4a63089cc38f733d0565
frk <vs1@frkeller.com>
2014-08-01 00:54:26 -0400

    Re-adding the main program who file name got changed.

---
+++ ConApp2/Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp3
+{
+    class ConApp3Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConApp3.ConApp3Class1.Main says Hello, world.");
+            Console.WriteLine("ConApp3.ConApp3Class1.Main says Press the Enter key: ");
+            Console.ReadLine();
+            ConApp3Class2.hello2();
+            ConApp3Class3.hello3();
+        }
+    }
+}
