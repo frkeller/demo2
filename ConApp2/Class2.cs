82c63a8c39b79648332e5913c0ce9aa780e4f68a
frk <vs1@frkeller.com>
2014-08-01 00:49:03 -0400

    Added Class2 and Class3, renamed the main class.

---
+++ ConApp2/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp3
+{
+    class ConApp3Class2
+    {
+        public static void hello2()
+        {
+            Console.WriteLine("ConApp3.ConApp3Class2.hello2 says Hello, world.");
+            Console.WriteLine("ConApp3.ConApp3Class2.hello2 says Press the Enter key: ");
+            Console.ReadLine();
+            ConApp3Class3.hello3();
+        }
+    }
+}
---
+++ ConApp2/Class3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp3
+{
+    class ConApp3Class3
+    {
+        public static void hello3()
+        {
+            Console.WriteLine("ConApp3.ConApp3Class2.hello3 says Hello, world.");
+            Console.WriteLine("ConApp3.ConApp3Class2.hello3 says Press the Enter key: ");
+            Console.ReadLine();
+        }
+    }
+}
